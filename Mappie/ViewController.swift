20753373829c17d0493f54a9822bb13620f76fcb
Roydon Jeffrey <brushdam14823@gmail.com>
2017-06-13 21:50:38 -0400

    Created and displayed a unique location on the map

--- Mappie/ViewController.swift
+++ Mappie/ViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Create a specific location for the map
+        let lat: CLLocationDegrees = 7.0
+        let long: CLLocationDegrees = 11.0
+        let latDelta: CLLocationDegrees = 0.01
+        let longDelta: CLLocationDegrees = 0.01
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
         
+        //Display the region on the map
+        mapView.setRegion(region, animated: true)
     }
     
 }
