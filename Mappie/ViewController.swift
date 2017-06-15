b33d8c3fe616332694104fccd9f4db309199b39a
Roydon Jeffrey <brushdam14823@gmail.com>
2017-06-14 23:34:33 -0400

    Added an annotation on the current location of the map with a title and subtitle

--- Mappie/ViewController.swift
+++ Mappie/ViewController.swift
@@ -16,17 +16,26 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
+        
         //Create a specific location for the map
-        let lat: CLLocationDegrees = 7.0
-        let long: CLLocationDegrees = 11.0
-        let latDelta: CLLocationDegrees = 0.01
-        let longDelta: CLLocationDegrees = 0.01
+        let lat: CLLocationDegrees = 43.095181
+        let long: CLLocationDegrees = -79.006424
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
         let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: lat, longitude: long)
         let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
         let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
         
         //Display the region on the map
-        mapView.setRegion(region, animated: true)
+        mapView.setRegion(region, animated: false)
+        
+        //Create an annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Current Location"
+        annotation.subtitle = "You are here now"
+        mapView.addAnnotation(annotation)
     }
     
 }
