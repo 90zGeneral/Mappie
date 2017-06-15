03de6401489800a39a59e651c3d767fba059f6a6
Roydon Jeffrey <brushdam14823@gmail.com>
2017-06-15 00:30:53 -0400

    User can now add more annotations for new locations by pressing on the map for 2 seconds

--- Mappie/ViewController.swift
+++ Mappie/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var number = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +37,31 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.coordinate = location
         annotation.title = "Current Location"
         annotation.subtitle = "You are here now"
+        mapView.addAnnotation(annotation)
+        
+        //Gesture Recognizer
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.lpgrAction(_:)))
+        longPressGestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    //Create a gesture recognizer
+    func lpgrAction(_ gestureRecognizer: UIGestureRecognizer) {
+        
+        print("gesture recognized")
+        
+        //Grab the user's touch point and convert it into coordinates
+        let userTouch = gestureRecognizer.location(in: mapView)
+        let newCoordinates: CLLocationCoordinate2D = mapView.convert(userTouch, toCoordinateFrom: mapView)
+        
+        //Increment the number count
+        number += 0.5
+        
+        //Create a new annotation for the new location
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        annotation.title = "New Location"
+        annotation.subtitle = "touch point \(Int(number))"
         mapView.addAnnotation(annotation)
     }
     
